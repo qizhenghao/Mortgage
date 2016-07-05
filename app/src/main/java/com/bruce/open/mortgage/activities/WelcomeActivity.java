819628e69dc4f731a2de31b431f01fc1cc78eaf6
qizhenghao <qizhenghao258@163.com>
2016-07-05 15:15:54 +0800

    add welcome ad

--- app/src/main/java/com/bruce/open/mortgage/activities/WelcomeActivity.java
+++ app/src/main/java/com/bruce/open/mortgage/activities/WelcomeActivity.java
@@ -4,6 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.widget.RelativeLayout;
+
+import com.baidu.mobads.SplashAd;
+import com.baidu.mobads.SplashAdListener;
+import com.bruce.open.mortgage.R;
 
 /**
  * Created by qizhenghao on 16/6/30.
@@ -13,9 +19,7 @@ public class WelcomeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-//        startActivity(new Intent(WelcomeActivity.this, DesktopActivity.class));
-//        WelcomeActivity.this.finish();
+        setContentView(R.layout.activity_welcome_layout);
 
 //        Timer timer = new Timer();
 //        TimerTask timerTask = new TimerTask() {
@@ -26,14 +30,52 @@ public class WelcomeActivity extends Activity {
 //            }
 //        };
 //        timer.schedule(timerTask, 3000);
-        new Handler().postDelayed(new Runnable() {
+
+        //                new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent = new Intent(WelcomeActivity.this, DesktopActivity.class);
+//                intent.putExtra("is_from_welcome", true);
+//                startActivity(intent);
+//                WelcomeActivity.this.finish();
+//            }
+//        }, 1500);
+        RelativeLayout adsParent = (RelativeLayout) this
+                .findViewById(R.id.welcome_activity_layout);
+        SplashAdListener listener = new SplashAdListener() {
             @Override
-            public void run() {
-                Intent intent = new Intent(WelcomeActivity.this, DesktopActivity.class);
-                intent.putExtra("is_from_welcome", true);
-                startActivity(intent);
-                WelcomeActivity.this.finish();
+            public void onAdDismissed() {
+                Log.i("RSplashActivity", "onAdDismissed");
+                jump2Desktop();
             }
-        }, 1500);
+
+            @Override
+            public void onAdFailed(String arg0) {
+                Log.i("RSplashActivity", "onAdFailed");
+                jump2Desktop();
+            }
+
+            @Override
+            public void onAdPresent() {
+                Log.i("RSplashActivity", "onAdPresent");
+            }
+
+            @Override
+            public void onAdClick() {
+                Log.i("RSplashActivity", "onAdClick");
+                //设置开屏可接受点击时,该回调可用
+                jump2Desktop();
+            }
+        };
+
+        String adPlaceId = "2673193";//重要:请填上你的代码位ID,否则无法请求到广告
+        new SplashAd(this, adsParent, listener, adPlaceId, true);
+    }
+
+    private void jump2Desktop() {
+        Intent intent = new Intent(WelcomeActivity.this, DesktopActivity.class);
+        intent.putExtra("is_from_welcome", true);
+        startActivity(intent);
+        WelcomeActivity.this.finish();
     }
 }
